0397d5f14a0031005c4858b01fc140e18979a5f8
Naing Lin Aung <nainglinaung@s2t.ai>
2023-02-01 21:00:59 +0630

    implementation for position

--- services/task/resolvers/mutation.ts
+++ services/task/resolvers/mutation.ts
@@ -2,7 +2,19 @@ import { Resolvers } from 'generated/types'
 import { Context } from '../../../libs/context'
 
 export const mutation: Resolvers<Context>['Mutation'] = {
-  createTask: async (_parent, { input }, ctx) => ctx.prisma.task.create({ data: input }),
+  createTask: async (_parent, { input }, ctx) => {
+
+    ctx.prisma.$use(async (params, next) => {
+      if (params.model == 'Task' && params.action == 'create') {
+        params.args.data.position = Date.now()
+      }
+      return next(params)
+    })
+
+    return ctx.prisma.task.create({ data: input })
+  },
+
+
   updateTask: async (_parent, { id, input }, ctx) =>
     ctx.prisma.task.update({
       where: { id },
