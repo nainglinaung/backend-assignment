76a339157bca05f02949a1584b965718881f0f20
Naing Lin Aung <nainglinaung@s2t.ai>
2023-02-01 14:30:51 +0630

    query list

--- services/list/resolvers/query.ts
+++ services/list/resolvers/query.ts
@@ -2,7 +2,13 @@ import { Resolvers } from 'generated/types'
 import { Context } from '../../../libs/context'
 
 export const query: Resolvers<Context>['Query'] = {
-  lists: async (_parent, _args, ctx) => ctx.prisma.list.findMany(),
+  lists: async (_parent, _args, ctx) => ctx.prisma.list.findMany({
+    include: {
+      Tasks: {
+        select: { name: true, position: true, id: true }
+      }
+    }
+  }),
   list: async (_parent, { id }, ctx) =>
     ctx.prisma.list.findUnique({
       where: { id },
