82b16888d7a36d67db3654fab4c5ed158548b328
Naing Lin Aung <nainglinaung@s2t.ai>
2023-02-02 14:21:16 +0630

    add moveTask test

--- test/Task.test.ts
+++ test/Task.test.ts
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from 'apollo-server'
+import { response } from 'express'
 import { createListServer } from "./createListServer"
 import { createTaskServer } from "./createTaskServer"
 
@@ -8,78 +9,114 @@ let listServer: ApolloServer
 let listId: String
 let taskId: String
 
-describe("Task Test Suite", () => {
-    beforeAll(async () => {
-        taskServer = await createTaskServer()
-        listServer = await createListServer()
+interface Input {
+  title: String
+  description: String
+  listId: String
+}
 
-        const query = gql`
+interface Checker {
+  id: String
+  position: Number
+}
+
+
+const getTask = async (checker: Checker) => {
+
+  const query = gql`
+  query Task($taskId: ID!) {
+    task(id: $taskId) {
+      position
+      id
+    }
+  }
+  `
+  return taskServer.executeOperation({
+    query,
+    variables: {
+      taskId: checker.id
+    },
+  });
+
+}
+
+const createNewTask = async (input: Input) => {
+
+  const query = gql`
+  mutation CreateTask($input: CreateTaskInput!) {
+      createTask(input: $input) {
+        title
+        description
+        listId
+        id
+        position
+      }
+    }
+  `
+
+  return taskServer.executeOperation({
+    query,
+    variables: {
+      input
+    },
+    operationName: "CreateTask"
+  });
+}
+
+describe("Task Test Suite", () => {
+  beforeAll(async () => {
+    taskServer = await createTaskServer()
+    listServer = await createListServer()
+
+    const query = gql`
         mutation CreateList($input: CreateListInput!) {
           createList(input: $input) {
             name,id
           }
         }
         `
-        const response = await listServer.executeOperation({
-            query,
-            variables: { input: { name: 'This is example list' } },
-            operationName: "CreateList"
-        });
-        listId = response.data?.createList?.id;
-    })
+    const response = await listServer.executeOperation({
+      query,
+      variables: { input: { name: 'This is example list' } },
+      operationName: "CreateList"
+    });
+    listId = response.data?.createList?.id;
+  })
 
-    afterAll(async () => {
-        const listquery = gql`
+  afterAll(async () => {
+    const listquery = gql`
         mutation DeleteList($deleteListId: ID!) {
           deleteList(id: $deleteListId) {
             success
           }
         }
       `
-        await listServer.executeOperation({
-            query: listquery,
-            variables: { deleteListId: listId },
-            operationName: "DeleteList"
-        });
+    await listServer.executeOperation({
+      query: listquery,
+      variables: { deleteListId: listId },
+      operationName: "DeleteList"
+    });
 
+  })
+
+  it('create new task from existing list"', async () => {
+
+
+    const response = await createNewTask({
+      title: "new task",
+      description: "hello this is new task",
+      listId
     })
+    taskId = response.data?.createTask?.id;
+    expect(response.errors).toBeUndefined();
+    expect(response.data?.createTask?.title).toBe('new task');
+    expect(response.data?.createTask?.description).toBe("hello this is new task");
+    expect(response.data?.createTask?.listId).toBe(listId);
 
-    it('create new task from existing list"', async () => {
+  })
 
-
-        const query = gql`
-        mutation CreateTask($input: CreateTaskInput!) {
-            createTask(input: $input) {
-              title
-              description
-              listId
-              id
-            }
-          }
-        `
-
-        const response = await taskServer.executeOperation({
-            query,
-            variables: {
-                input: {
-                    title: "new task",
-                    description: "hello this is new task",
-                    listId
-                }
-            },
-            operationName: "CreateTask"
-        });
-
-        taskId = response.data?.createTask?.id;
-        expect(response.errors).toBeUndefined();
-        expect(response.data?.createTask?.title).toBe('new task');
-        expect(response.data?.createTask?.description).toBe("hello this is new task");
-        expect(response.data?.createTask?.listId).toBe(listId);
-
-    })
-
-    it('edit change the task title,status and description"', async () => {
-        const query = gql`
+  it('edit change the task title,status and description"', async () => {
+    const query = gql`
         mutation UpdateTask($updateTaskId: ID!, $input: UpdateTaskInput!) {
             updateTask(id: $updateTaskId, input: $input) {
               id
@@ -92,43 +129,94 @@ describe("Task Test Suite", () => {
           }
           `
 
-        const response = await taskServer.executeOperation({
-            query,
-            variables: {
-                input: {
-                    title: "updated task",
-                    description: "updated description",
-                },
-                updateTaskId: taskId
-            },
-            operationName: "UpdateTask"
-        });
+    const response = await taskServer.executeOperation({
+      query,
+      variables: {
+        input: {
+          title: "updated task",
+          description: "updated description",
+        },
+        updateTaskId: taskId
+      },
+      operationName: "UpdateTask"
+    });
 
-        expect(response.errors).toBeUndefined();
-        expect(response.data?.updateTask?.title).toBe('updated task');
-        expect(response.data?.updateTask?.description).toBe("updated description");
-        expect(response.data?.updateTask?.listId).toBe(listId);
+    expect(response.errors).toBeUndefined();
+    expect(response.data?.updateTask?.title).toBe('updated task');
+    expect(response.data?.updateTask?.description).toBe("updated description");
+    expect(response.data?.updateTask?.listId).toBe(listId);
 
-    })
+  })
 
-    it("delete task by id", async () => {
-        const query = gql`
+  it("delete task by id", async () => {
+    const query = gql`
         mutation DeleteTask($deleteTaskId: ID!) {
             deleteTask(id: $deleteTaskId) {
               success
             }
           }
         `
-        const response = await taskServer.executeOperation({
-            query,
-            variables: { deleteTaskId: taskId },
-            operationName: "DeleteTask"
-        });
+    const response = await taskServer.executeOperation({
+      query,
+      variables: { deleteTaskId: taskId },
+      operationName: "DeleteTask"
+    });
 
-        expect(response.errors).toBeUndefined();
-        expect(response.data?.deleteTask?.success).toBe(true);
+    expect(response.errors).toBeUndefined();
+    expect(response.data?.deleteTask?.success).toBe(true);
 
-    })
+  })
+
+  it("move tasks", async () => {
+
+    let idArray = [];
+
+    for (let i = 0; i < 5; i++) {
+      const response = await createNewTask({
+        title: `task title  -${i}`,
+        description: `hello this is new task with description from ${i}`,
+        listId
+      });
+
+      let checker: Checker = {
+        id: response.data?.createTask?.id,
+        position: response.data?.createTask?.position
+      }
+      idArray.push(checker)
+    }
+
+    let moveFrom: Checker = idArray[0];
+    let moveTo: Checker = idArray[3];
+
+    const query = gql`
+    mutation MoveTask($moveTaskId: ID!, $input: MoveTaskInput!) {
+      moveTask(id: $moveTaskId, input: $input) {
+        success
+      }
+    }
+    `
+    const response = await taskServer.executeOperation({
+      query,
+      variables: {
+        moveTaskId: moveFrom.id, input: {
+          moveTo: moveTo.id
+        }
+      },
+      operationName: "MoveTask"
+    });
+
+    expect(response.errors).toBeUndefined();
+    expect(response.data?.moveTask?.success).toBe(true);
+
+    let newMoveTo = await getTask(moveTo)
+    let newMoveFrom = await getTask(moveFrom)
+
+    expect(newMoveTo.data?.task?.position).toBe(moveFrom.position);
+    expect(newMoveFrom.data?.task?.position).toBe(moveTo.position);
+
+
+
+  })
 
 
 
